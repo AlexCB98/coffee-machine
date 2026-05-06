a503c4769c663e7ddfac40b1f4783302412a5218
Alexandru <lexyus22@gmail.com>
2026-05-06 17:31:23 +0200

    Create report formatting function.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from data import MENU, resources
 
+def format_report_resource(resource):
+    water = resource['water']
+    milk = resource['milk']
+    coffee = resource['coffee']
+
+    return f'Water: {water} ml\nMilk: {milk}ml\nCoffee: {coffee}g'
+
