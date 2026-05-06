61f4e40889a276b783fbc5545eaeb393bded2ac4
Alexandru <lexyus22@gmail.com>
2026-05-06 17:35:16 +0200

    Add basic coffee machine controls.

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ def format_report_resource(resource):
 
     return f'Water: {water} ml\nMilk: {milk}ml\nCoffee: {coffee}g'
 
+while True:
+    prompt = input('What would you like? (expresso / latte / cappuccino ): ').lower()
+
+    if prompt == 'report'.lower():
+        print(format_report_resource(resources))
+
+
+
+
+
+    if prompt == 'off'.lower():
+        break
