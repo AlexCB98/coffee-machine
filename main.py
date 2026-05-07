113fec2ef2e8b59f1213a75e60d35023b80d074f
Alexandru <lexyus22@gmail.com>
2026-05-07 16:07:44 +0200

    Implement resource validation.

--- main.py
+++ main.py
@@ -4,11 +4,34 @@ def format_report_resource(resource):
     water = resource['water']
     milk = resource['milk']
     coffee = resource['coffee']
+    money = resource['money']
 
-    return f'Water: {water} ml\nMilk: {milk}ml\nCoffee: {coffee}g'
+    return f'Water: {water} ml\nMilk: {milk} ml\nCoffee: {coffee} g\nMoney: $ {money}'
+
+def check_resources(resource, drink):
+    r_water = resource['water']
+    r_milk = resource['milk']
+    r_coffee = resource['coffee']
+    d_water = drink['water']
+    d_milk = drink['milk']
+    d_coffee = drink['coffee']
+
+    if r_water >= d_water and r_milk >= d_milk and r_coffee >= d_coffee:
+        return True
+    elif r_water < d_water:
+        print('There is not enough water.')
+        return False
+    elif r_milk < d_milk:
+        print('There is not enough milk.')
+        return False
+    else:
+        print('There is not enough coffee.')
+        return False
+
+
 
 while True:
-    prompt = input('What would you like? (expresso / latte / cappuccino ): ').lower()
+    prompt = input('What would you like? (espresso / latte / cappuccino ): ').lower()
 
     if prompt == 'report'.lower():
         print(format_report_resource(resources))
